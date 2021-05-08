5b246c662371c5454428c9974c05453f77f45eb0
Augs Machado <augustomachadoramos@gmail.com>
2021-05-08 12:30:34 -0300

    .

---
+++ client/target/debug/client.d
@@ -0,0 +1,1 @@
+/Users/augustomachado/github/rust_chat_application/client/target/debug/client: /Users/augustomachado/github/rust_chat_application/client/src/main.rs
---
+++ client/target/debug/deps/client.d
@@ -0,0 +1,5 @@
+/Users/augustomachado/github/rust_chat_application/client/target/debug/deps/client: src/main.rs
+
+/Users/augustomachado/github/rust_chat_application/client/target/debug/deps/client.d: src/main.rs
+
+src/main.rs:
